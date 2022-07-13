b2306c6ca0b4178fcbaffa6bfdb834b9092c7961
Michael Lodder <redmike7@gmail.com>
2022-07-13 09:12:38 -0600

    update php
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- rust/src/php.rs
+++ rust/src/php.rs
@@ -9,13 +9,13 @@ use rand::thread_rng;
 
 use ext_php_rs::zend::ModuleEntry;
 use ext_php_rs::{info_table_end, info_table_row, info_table_start};
-use std::{convert::TryFrom, vec::Vec};
+use std::{convert::TryFrom, vec::Vec, string::String};
 
 /// Create a new secret key
 ///
 /// @return string The secret key
 #[php_function]
-pub fn new_secret_key() -> Vec<u8> {
+pub fn oberon_new_secret_key() -> Vec<u8> {
     let rng = thread_rng();
     SecretKey::new(rng).to_bytes().to_vec()
 }
@@ -26,7 +26,7 @@ pub fn new_secret_key() -> Vec<u8> {
 ///
 /// @return string The public key
 #[php_function]
-pub fn get_public_key(sk: Vec<u8>) -> Option<Vec<u8>> {
+pub fn oberon_get_public_key(sk: Vec<u8>) -> Option<Vec<u8>> {
     match secret_key(sk) {
         None => None,
         Some(sk) => Some(PublicKey::from(&sk).to_bytes().to_vec()),
@@ -39,7 +39,7 @@ pub fn get_public_key(sk: Vec<u8>) -> Option<Vec<u8>> {
 ///
 /// @return string The secret key
 #[php_function]
-pub fn secret_key_from_seed(seed: Vec<u8>) -> Vec<u8> {
+pub fn oberon_secret_key_from_seed(seed: Vec<u8>) -> Vec<u8> {
     SecretKey::hash(&seed).to_bytes().to_vec()
 }
 
@@ -50,10 +50,10 @@ pub fn secret_key_from_seed(seed: Vec<u8>) -> Vec<u8> {
 ///
 /// @return string|null The token if successful, null on failure
 #[php_function]
-pub fn new_token(sk: Vec<u8>, id: Vec<u8>) -> Option<Vec<u8>> {
+pub fn oberon_new_token(sk: Vec<u8>, id: String) -> Option<Vec<u8>> {
     match secret_key(sk) {
         None => None,
-        Some(sk) => Some(Token::new(&sk, &id).unwrap().to_bytes().to_vec()),
+        Some(sk) => Some(Token::new(&sk, id.as_bytes()).unwrap().to_bytes().to_vec()),
     }
 }
 
@@ -65,9 +65,9 @@ pub fn new_token(sk: Vec<u8>, id: Vec<u8>) -> Option<Vec<u8>> {
 ///
 /// @param bool result of check
 #[php_function]
-pub fn verify_token(token: Vec<u8>, pk: Vec<u8>, id: Vec<u8>) -> bool {
+pub fn oberon_verify_token(token: Vec<u8>, pk: Vec<u8>, id: String) -> bool {
     match (get_token(token), public_key(pk)) {
-        (Some(t), Some(k)) => t.verify(k, &id).unwrap_u8() == 1,
+        (Some(t), Some(k)) => t.verify(k, id.as_bytes()).unwrap_u8() == 1,
         (_, _) => false,
     }
 }
@@ -78,7 +78,7 @@ pub fn verify_token(token: Vec<u8>, pk: Vec<u8>, id: Vec<u8>) -> bool {
 ///
 /// @param string The blinding factor
 #[php_function]
-pub fn create_blinding(data: Vec<u8>) -> Vec<u8> {
+pub fn oberon_create_blinding(data: Vec<u8>) -> Vec<u8> {
     Blinding::new(&data).to_bytes().to_vec()
 }
 
@@ -89,7 +89,7 @@ pub fn create_blinding(data: Vec<u8>) -> Vec<u8> {
 ///
 /// @param string|null The blinding factor or null if invalid token
 #[php_function]
-pub fn add_blinding(token: Vec<u8>, data: Vec<u8>) -> Option<Vec<u8>> {
+pub fn oberon_add_blinding(token: Vec<u8>, data: Vec<u8>) -> Option<Vec<u8>> {
     match get_token(token) {
         None => None,
         Some(t) => {
@@ -106,7 +106,7 @@ pub fn add_blinding(token: Vec<u8>, data: Vec<u8>) -> Option<Vec<u8>> {
 ///
 /// @param string The blinding factor or null if invalid token
 #[php_function]
-pub fn remove_blinding(token: Vec<u8>, data: Vec<u8>) -> Option<Vec<u8>> {
+pub fn oberon_remove_blinding(token: Vec<u8>, data: Vec<u8>) -> Option<Vec<u8>> {
     match get_token(token) {
         None => None,
         Some(t) => {
@@ -125,9 +125,9 @@ pub fn remove_blinding(token: Vec<u8>, data: Vec<u8>) -> Option<Vec<u8>> {
 ///
 /// @return string|null The proof is successful, null on failure
 #[php_function]
-pub fn create_proof(
+pub fn oberon_create_proof(
     token: Vec<u8>,
-    id: Vec<u8>,
+    id: String,
     blindings: Vec<Vec<u8>>,
     nonce: Vec<u8>,
 ) -> Option<Vec<u8>> {
@@ -136,7 +136,7 @@ pub fn create_proof(
     let rng = thread_rng();
     match get_token(token) {
         None => None,
-        Some(t) => Proof::new(&t, &bs, id, nonce, rng).map(|p| p.to_bytes().to_vec()),
+        Some(t) => Proof::new(&t, &bs, id.as_bytes(), nonce, rng).map(|p| p.to_bytes().to_vec()),
     }
 }
 
@@ -149,9 +149,9 @@ pub fn create_proof(
 ///
 /// @return bool The check proof result
 #[php_function]
-pub fn verify_proof(proof: Vec<u8>, pk: Vec<u8>, id: Vec<u8>, nonce: Vec<u8>) -> bool {
+pub fn oberon_verify_proof(proof: Vec<u8>, pk: Vec<u8>, id: String, nonce: Vec<u8>) -> bool {
     match (get_proof(proof), public_key(pk)) {
-        (Some(p), Some(k)) => p.open(k, id, nonce).unwrap_u8() == 1,
+        (Some(p), Some(k)) => p.open(k, id.as_bytes(), nonce).unwrap_u8() == 1,
         (_, _) => false,
     }
 }
